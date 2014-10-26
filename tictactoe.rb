bca40799109c8c978fe97795c6fa9e8229dbf196
Ben Stein <bumstain@gmail.com>
2014-10-26 23:51:00 +0000

    Updated gameplay and identification of winner

--- tictactoe.rb
+++ tictactoe.rb
@@ -30,7 +30,6 @@
 # Deadlock 
 
 # Loop until a winner is found, or all squares are taken
-require 'pry'
 
 def initialize_board
 	b = {}
@@ -39,24 +38,57 @@ def initialize_board
 end
 
 def draw_board(b)
+	system 'clear'
+	puts "------------------"
 	puts "  #{b[1]}  |  #{b[2]}  |  #{b[3]}"
 	puts "------------------"
 	puts "  #{b[4]}  |  #{b[5]}  |  #{b[6]}"
 	puts "------------------"
 	puts "  #{b[7]}  |  #{b[8]}  |  #{b[9]}"
+	puts "------------------"
+end
+
+def empty_positions(b)
+	b.select {|k, v| v == ' '}.keys
 end
 
 def player_picks_square(b)
 	puts "Please pick a square (1-9)."
 	position = gets.chomp.to_i
 	b[position] = 'X'
-	binding.pry
 end
 
+def computer_picks_square(b)
+	position = empty_positions(b).sample
+	b[position] = 'O'
+end
+
+def check_winner(b)
+	winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+	winning_lines.each do |line|
+		if b[line[0]] == 'X' and b[line[1]] == 'X' and b[line[2]] == 'X'
+			return 'Player'
+		elsif b[line[0]] == 'X' and b[line[1]] == 'X' and b[line[2]] == 'X'
+			return 'Computer'
+		else 
+			return nil
+		end
+	end
+end
+
+
 board = initialize_board
-
 draw_board(board)
-	player_picks_square(board)
+	
 begin 
+	player_picks_square(board)
+	computer_picks_square(board)
+	draw_board(board)
+	winner = check_winner(board)
+end until winner || empty_positions(board).empty?
 
-end until winner || all_squares_taken?
+if winner
+	"#{winner} won!"
+else
+	puts "It's a tie!"
+end
